e5324c24675ebbb039cca4aaf99130e3b008e4ac
Raz Haleva <razhaleva@Raz-MacBook-Pro.local>
2016-11-10 20:04:05 +0200

    Organized FS just a bit more

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MaterialModule } from '@angular/material';
-import { AppComponent }   from './app.component';
+import { AppComponent }   from './components/app.component';
 
 @NgModule({
   imports:      [ BrowserModule, MaterialModule.forRoot() ],
--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular App</h1>    <button md-raised-button color="primary">FLAT</button>'
+  templateUrl: 'app/views/app.html'
 })
 export class AppComponent { }
