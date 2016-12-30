d413fa2d0d38b3b29b4e06909a07a8d3e013b5e6
Raz Haleva <razhaleva@gmail.com>
2016-12-30 02:41:31 +0200

    Removed unnecessary usage of class-transformer

--- src/api/models/noun.model.ts
+++ src/api/models/noun.model.ts
@@ -1,8 +1,6 @@
 import { Gender } from "./globals.model";
-import { Type } from "class-transformer"
 
 export class Noun {
-    @Type(() => Boolean)
     favorite: boolean;
     gender: Gender;
     english: string;
