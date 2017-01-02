47a0473d6d3bfdf9c879209cf28b9924d25a3f3c
Raz Haleva <razhaleva@gmail.com>
2017-01-02 22:51:02 +0200

    Supporting deployments in Heroku * Explicitly defined node & npm versions in package.json * Defined 'start', and 'postinstall' scripts in package.json * Server listens on the port Heroku will provide (by using the PORT environment variable if exists)

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,16 +1,18 @@
 import "reflect-metadata";
 import { createExpressServer } from "routing-controllers";
 import * as express from "express";
+import { join } from "path";
 
-console.log("Dictio running under '" + __dirname + "/../app/app'");
+let port = process.env.PORT || 8080;
+let path = join(__dirname, "../app/app");
 
 let app = createExpressServer({
   controllers: [__dirname + "/controllers/**/*.js"],
   routePrefix: "/rest",
 });
 
-app.use(express.static('../app/app'));
+app.use(express.static(path));
 
-app.listen(3000, function () {
-  console.log("Dictio is listening on port 3000!");
+app.listen(port, function () {
+  console.log("Dictio is running [port " + port + "] under '" + path + "'");
 });
