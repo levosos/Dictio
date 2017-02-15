cf5c3442dd4ff575fa2407d2efcd3dcd5294977f
Raz Haleva <razhaleva@gmail.com>
2017-02-16 00:03:10 +0200

    Using materializecss * Totally re-wrote main component's view * Currently, it has no side-nav * Improved the way we generate the footer * Removed unnecessary types from .scss

--- src/app/components/main.component.ts
+++ src/app/components/main.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 import { TokenService } from '../services/token.service';
-import { UtilsService } from '../services/utils.service';
 
 @Component({
   templateUrl: 'views/main.component.html'
 })
 export class MainComponent { 
-    constructor(private tokenService: TokenService,
-                private utils: UtilsService) {
-    }
+  constructor(private tokenService: TokenService) {
+  }
+
+  get sections(): any[] {
+    return [
+      { name: "Navigate", items: [
+        { name: 'index', url: '#' },
+        this.tokenService.isLoggedIn() ? 
+          { name: 'logout', url: '#/logout' } : 
+          { name: 'login', url: '#/login' }
+      ]},
+      { name: 'View', items: [
+        { name: "Nouns", url: "#/nouns" },
+        { name: "Adjectives", url: "#/adjectives" },
+        { name: "Verbs", url: "#/verbs" },
+        { name: "Words", url: "#/conjunctions" }
+      ]}
+    ];
+  }
 }
