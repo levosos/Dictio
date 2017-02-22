ca3007581a4c3878c906e215de889541131c8775
Raz Haleva <razhaleva@gmail.com>
2017-02-22 20:14:11 +0200

    Using pug for all views

--- src/app/components/main.component.ts
+++ src/app/components/main.component.ts
@@ -10,17 +10,17 @@ export class MainComponent {
 
   get sections(): any[] {
     return [
-      { name: "Navigate", items: [
+      { name: 'Navigate', items: [
         { name: 'index', url: '#' },
         this.tokenService.isLoggedIn() ? 
           { name: 'logout', url: '#/logout' } : 
           { name: 'login', url: '#/login' }
       ]},
       { name: 'View', items: [
-        { name: "Nouns", url: "#/nouns" },
-        { name: "Adjectives", url: "#/adjectives" },
-        { name: "Verbs", url: "#/verbs" },
-        { name: "Words", url: "#/conjunctions" }
+        { name: 'Nouns', url: '#/nouns' },
+        { name: 'Adjectives', url: '#/adjectives' },
+        { name: 'Verbs', url: '#/verbs' },
+        { name: 'Words', url: '#/conjunctions' }
       ]}
     ];
   }
